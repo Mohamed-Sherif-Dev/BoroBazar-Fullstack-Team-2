e4951fdfbbe583121cd288e8bfe1dc8830f1b873
AbdElRahman-7 <abdelrahmanhamdy1999@gmail.com>
2026-03-15 14:53:13 +0200

    fix Conflict

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 import { createRoot } from "react-dom/client";
+import './index.css'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import App from "./App.tsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
@@ -15,15 +13,5 @@ createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </QueryClientProvider>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> dashboard
+  
